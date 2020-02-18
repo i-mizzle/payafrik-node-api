ee46cf3b38b46da85312cbc65e8d9c9287a7327b
Immanuel Onum <immanuel.o.onum@gmail.com>
2020-02-18 15:35:39 +0100

    updated swagger live url - bug fix

--- src/swagger.js
+++ src/swagger.js
@@ -21,11 +21,11 @@ module.exports = {
       description: 'Local server'
     },
     {
-      url: 'http://ec2-3-16-42-184.us-east-2.compute.amazonaws.com:/v1/api',
+      url: 'http://ec2-3-16-42-184.us-east-2.compute.amazonaws.com:5000/v1/api',
       description: 'Testing server'
     },
     {
-      url: 'http://ec2-3-16-42-184.us-east-2.compute.amazonaws.com:/v1/api',
+      url: 'http://ec2-3-16-42-184.us-east-2.compute.amazonaws.com:5000/v1/api',
       description: 'Production server'
     }
   ],
